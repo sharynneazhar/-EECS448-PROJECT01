05f1a8d05897787c763fee5453b62367e21b8d56
Andrew Thomas <drew_thomas@ku.edu>
2016-09-10 19:59:01 -0500

    Initial commit. Started main.cpp, added README.txt for communications

---
+++ main.cpp
@@ -0,0 +1,25 @@
+/**
+* @author Andrew Thomas, Katie Hronchir, Mac Crider, Yehan Li
+* @file main.cpp
+* @since 10 September 2016
+* @brief Main runtime of the Calendar project.
+*/
+
+#include <iostream>
+#include <string>
+
+
+int main (int argc, char** argv)
+{
+  bool repeat = true;
+  cout<<"Welcome to KAMYcal"<<endl;
+  while (repeat)
+  {
+    cout<<"Please choose a month: ";
+    string month;
+    cin>>month;
+    //switch based off of first character of string to set month
+  }
+
+
+}
