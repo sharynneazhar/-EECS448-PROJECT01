a91ab004c2c9734ac9992291c76463c292a1d350
Yehan Li <greatyehanli@gmail.com>
2016-09-14 00:23:46 -0500

    Changed date variable to day variable, added a month variable, added a DateTest function to check user's input

--- Day.cpp
+++ Day.cpp
@@ -1,6 +1,6 @@
 #include "Day.h"
 
-Day:: Day(const int Date, int Year, char Week, bool Note)
+Day:: Day(const int Date, int Year, int Week, bool Note)
 {
     date=Date;
     week=week;
@@ -16,7 +16,7 @@ Day:: Day()
     year=0;
 }
 
-void Day:: setDate(int Date)
+void Day:: setDay(int Date)
 {
     date=Date;
 }
@@ -36,12 +36,17 @@ void Day:: setYear(int Year)
     year=Year;
 }
 
+void Day:: setMonth(string Month)
+{
+    month=Month;
+}
+
 int Day:: getYear()
 {
     return(year);
 }
 
-int Day:: getDate() 
+int Day:: getDay() 
 {
     return(date);
 }
@@ -51,7 +56,70 @@ char Day:: getWeek()
     return(week);
 }
 
+string Day:: getMonth()
+{
+    return month;
+}
+
 bool Day:: getNote()
 {
     return(note);
+}
+
+bool Day:: DateTest(string month, int day, int year)
+{
+   if(day>0 && day<32 && year>2015 && year<2018)
+  {
+    if(month=="Aug")
+    {
+        return true;
+    }
+    else if(month=="Sep")
+    {
+        return true; 
+    }
+    else if(month=="Oct")
+    {
+        return true;
+    }
+    else if(month=="Nov")
+    {
+        return true; 
+    }
+     else if(month=="Dec")
+    {
+        return true;
+    }
+    else if(month=="Jan")
+    {
+        return true; 
+    }
+     else if(month=="Feb")
+    {
+        return true;
+    }
+    else if(month=="Mar")
+    {
+        return true; 
+    }
+     else if(month=="Apr")
+    {
+        return true;
+    }
+    else if(month=="May")
+    {
+        return true; 
+    }
+    else
+    {
+        return false;
+        cout<<"Please input the month as what is demonstrated in the examples."<<endl;
+    }
+    //check the format of user's input of Current date.
+  }
+  else
+  {
+      cout<<"Please input valid day and year."<<endl;
+      return false;
+  }
 }
