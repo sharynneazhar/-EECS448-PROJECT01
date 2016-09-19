0f9943471c39491269e938089391f9c7f3e1b6fe
Mac Crider <crider.mac@gmail.com>
2016-09-18 20:27:06 -0500

    test

--- Day.h
+++ Day.h
@@ -1,6 +1,5 @@
 #ifndef DAY_H
 #define DAY_H
-
 #include <string>
 #include <iostream>
 #include <fstream>
@@ -12,21 +11,18 @@ class Day
     int year;//year
     char week;// day of the week
     bool note;//Does it have a note/description?
-    
+
     public:
     Day();
-    Day(const int Date, const int Year, char Week, bool note);
+    Day(const int Date, char Week, bool note);
     void setDate(const int Date);
     void setWeek(const char Week);
-    void setYear(const int Year);
     void setNote(bool note);
     int getDate();
     char getWeek();
-    int getYear();
-
     bool getNote();
     //set and get functions
-    
+
 };
 
 #endif
