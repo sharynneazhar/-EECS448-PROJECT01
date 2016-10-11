240b65befb5c4d11869635ed75ee01f71bc39b8c
erincoots <e.coots218@gmail.com>
2016-10-10 21:45:22 -0500

    Event class

---
+++ Event.cpp
@@ -0,0 +1,91 @@
+/**
+* @author Erin Coots
+* @file event.cpp
+* @since 10 October 2016
+* @brief Functionality of the event class.
+*/
+
+#include "Event.h"
+using namespace std;
+
+Event::Event(){
+    name = "";
+    description = "";
+    startTime = 0;
+    endTime = 0;
+}
+
+Event::Event(string n, string desc, const int sd, const int sy, string sm,
+  const int ed, const int ey, string em, const int start, const int end){
+    name = n;
+    description = desc;
+    startDate.day = sd;
+    startDate.month = sm;
+    startDate.year = sy;
+    endDate.day = ed;
+    endDate.month = em;
+    endDate.year = ey;
+    startTime = start;
+    endTime = end;
+}
+
+void Event::setName(string n){
+    name = n;
+}
+
+string Event::getName(){
+    return name;
+}
+
+void Event::setDesc(string desc){
+    description = desc;
+}
+
+string Event::getDesc(){
+    return description;
+}
+
+void Event::setStartDate(int day, string month, int year){
+    startDate.day = day;
+    startDate.month = month;
+    startDate.year = year;
+}
+
+struct Date Event::getStartDate(){
+    return startDate;
+}
+
+void Event::setEndDate(int day, string month, int year){
+    endDate.day = day;
+    endDate.month = month;
+    endDate.year = year;
+}
+
+struct Date Event::getEndDate(){
+    return endDate;
+}
+
+void Event::setStartTime(const int start){
+    startTime = start;
+}
+
+int Event::getStartTime(){
+    return startTime;
+}
+
+void Event::setEndTime(const int end){
+    endTime = end;
+}
+
+int Event::getEndTime(){
+    return endTime;
+}
+
+bool Event::isEmpty(){
+    if(name == "" || name == null){
+      return true;
+    }
+    else{
+      return false;
+    }
+}
---
+++ Event.h
@@ -0,0 +1,142 @@
+/**
+* @author Erin Coots
+* @file Event.h
+* @since 10 October 2016
+* @brief Header of the Event class.
+*/
+
+#ifndef EVENT_H
+#define EVENT_H
+#include <string>
+#include <iostream>
+using namespace std;
+#include <fstream>
+
+struct Date{
+  int day;
+  string month;
+  int year;
+}
+
+class Event{
+
+    private:
+      string name;
+      string description;
+      struct Date startDate;
+      struct Date endDate;
+      int startTime;
+      int endTime;
+
+    public:
+      /**
+      *  @pre None.
+      *  @post Constructs a Event class object with empty name and description,
+      *        empty start and end date, 0 start and end times
+      *  @return Event class object
+      */
+      Event();
+
+      /**
+      *  @pre None.
+      *  @post Constructs a Event class object with the given arguments for name,
+      *        description, start and end dates, start and end times
+      *  @return Event class object
+      */
+      Event(string n, string desc, const int sd, const int sy, string sm,
+        const int ed, const int ey, string em, const int start, const int end);
+
+      /**
+      *  @pre None.
+      *  @post Sets the name variable.
+      *  @return None.
+      */
+      void setName(string n);
+
+      /**
+      *  @pre None.
+      *  @post None.
+      *  @return Name value
+      */
+      string getName();
+
+      /**
+      *  @pre None.
+      *  @post Sets the description variable.
+      *  @return None.
+      */
+      void setDesc(string desc);
+
+      /**
+      *  @pre None.
+      *  @post None.
+      *  @return Description value
+      */
+      string getDesc();
+
+      /**
+      *  @pre None.
+      *  @post Sets the start date variable.
+      *  @return None.
+      */
+      void setStartDate(int day, string month, int year);
+
+      /**
+      *  @pre None.
+      *  @post None.
+      *  @return Start date value
+      */
+      struct Date getStartDate();
+
+      /**
+      *  @pre None.
+      *  @post Sets the end date variable.
+      *  @return None.
+      */
+      void setEndDate(int day, string month, int year);
+
+      /**
+      *  @pre None.
+      *  @post None.
+      *  @return End date value
+      */
+      struct Date getEndDate();
+
+      /**
+      *  @pre None.
+      *  @post Sets the start time variable.
+      *  @return None.
+      */
+      void setStartTime(const int start);
+
+      /**
+      *  @pre None.
+      *  @post None.
+      *  @return Start time value
+      */
+      int getStartTime();
+
+      /**
+      *  @pre None.
+      *  @post Sets the end time variable.
+      *  @return None.
+      */
+      void setEndTime(const int end);
+
+      /**
+      *  @pre None.
+      *  @post None.
+      *  @return End time value
+      */
+      int getEndTime();
+
+      /**
+      *  @pre None.
+      *  @post None.
+      *  @return Bool for if the Event is empty
+      */
+      bool isEmpty();
+
+};
+
+#endif
